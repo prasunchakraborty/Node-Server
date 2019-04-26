f1fcb4bb2c771645560e287f68e40bcd18763072
Prasun Chakraborty <prasunatwork@gmail.com>
2019-04-26 19:38:43 +0530

    Updated user model and CRUD for the movies API

--- server/models/users.js
+++ server/models/users.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator")
 let schema = mongoose.Schema({
-    Name :{
+    name :{
         type: String,
         require: true,
     },
-    Email:{
+    email:{
+        type: String,
+        require:true,
+        trim:true,
+        unique:true,
+        validate:{
+            validator: validator.isEmail,
+            message: `{VALUE} is not a valid email`
+        }
+    },
+    password:{
         type: String,
         require:true
-    }
+    },
+    tokens:[{
+        access:{
+            type:String,
+            require:true
+        },
+        token:{
+            type:String,
+            require:true
+        }
+    }]
 });
 
-let User = mongoose.model("User",schema);
-module.exports = {User};
+let Users = mongoose.model("User",schema);
+module.exports = {Users};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
+const {ObjectID} = require("mongodb");
 
 const {mongoose} = require("./db/mongoose");
 const {Users} = require("./models/users");
 const {Movies} = require("./models/movies");
+
+const PORT = process.env.PORT || 3000
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,12 +22,89 @@ app.post("/movies",(req,res)=>{
     user.save((err,resp)=>{
         if(err){
             console.log(`${err["name"]}: ${err["message"]}`);
-            res.send(`${err["name"]}: ${err["message"]}`);
+            res.status(400).send(`${err["name"]}: ${err["message"]}`);
         }
         res.send(resp);
-    })
+    });
 });
 
-app.listen(3000,()=>{
-    console.log("The App is running on port 3000");
+app.get("/movies",(req,res)=>{
+    Movies.find((err,resp)=>{
+        if(err){
+            res.status(400).send(err);
+        };
+        res.send({resp})
+    });
+});
+
+app.get("/movies/:id",(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    Movies.findById(req.params.id,(err,resp)=>{
+        if(err){
+            res.status(400).send(err);
+        };
+        if(!resp){
+            return res.status(404).send();
+        };
+        res.send({resp});
+    });
+});
+
+app.delete("/movies/:id",(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    };
+    Movies.findByIdAndDelete(req.params.id,(err,resp)=>{
+        if(err){
+            res.status(400).send(err);
+        };
+        if(!resp){
+            return res.status(404).send();
+        };
+        res.send({resp});
+    });
+
+});
+
+app.patch("/movies/:id",(req,res)=>{
+    let body = _.pick(req.body,['watched']);
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+    
+    if (!_.isBoolean(body.watched)){
+        body.Watched=false;
+    }
+    else{
+        body.Watched = body.watched;
+    }
+    
+    Movies.findByIdAndUpdate(id,body,{new: true},(err,resp)=>{
+        if(err){
+            res.status(400).send(err);
+        };
+        if(!resp){
+            return res.status(404).send();
+        };
+        res.send({resp});
+    });
+
+});
+
+app.post("/users",(req,res)=>{
+    let body = _.pick(req.body,['name','email','password'])
+    let user = new Users(body);
+    user.save((err,resp)=>{
+        if(err){
+            res.status(400).send(err);
+        };
+        res.send(resp);
+    });
+});
+
+app.listen(PORT,()=>{
+    console.log(`The App is running on port ${PORT}`);
 });
